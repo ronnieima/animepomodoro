6f6fd99243cdaef3073281f0299c49e584a43574
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-12-15 16:48:47 +0100

    add context

---
+++ app/_components/ui/Timer.tsx
@@ -0,0 +1,44 @@
+import { useTimerContext } from "@/app/_contexts/TimerContext";
+import React, { useState } from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+function Timer() {
+  const { toggleTimer, isPlaying, time } = useTimerContext();
+  return (
+    <CountdownCircleTimer
+      onComplete={() => {
+        toggleTimer;
+        console.log(isPlaying);
+        return { shouldRepeat: false };
+      }}
+      isPlaying={isPlaying}
+      size={360}
+      duration={time}
+      strokeWidth={32}
+      colors="#004777"
+    >
+      {renderTime}
+    </CountdownCircleTimer>
+  );
+}
+
+const renderTime = ({ remainingTime }: { remainingTime: number }) => {
+  const minutes = Math.floor((remainingTime % 3600) / 60);
+  const seconds = remainingTime % 60;
+  function str_pad_left(string: string, pad: string, length: number) {
+    return (new Array(length + 1).join(pad) + string).slice(-length);
+  }
+
+  const finalTime =
+    str_pad_left(minutes.toString(), "0", 2) +
+    ":" +
+    str_pad_left(seconds.toString(), "0", 2);
+
+  return (
+    <div className="timer">
+      <div className="text-6xl font-semibold">{finalTime}</div>
+    </div>
+  );
+};
+
+export default Timer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,14 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
-import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import ThemeToggle from "@/app/_components/ui/ThemeToggle";
 import TypeTags from "./_components/ui/TypeTabs";
-
-const renderTime = ({ remainingTime }: { remainingTime: number }) => {
-  return (
-    <div className="timer">
-      <div className="text-6xl font-semibold">{remainingTime}</div>
-    </div>
-  );
-};
+import Timer from "./_components/ui/Timer";
+import { useCountdown } from "react-countdown-circle-timer";
+import { useTimerContext } from "./_contexts/TimerContext";
 
 export default function Home() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [shortBreakCount, setShortBreakCount] = useState(0);
-  const [key, setKey] = useState(0);
-
+  const { toggleTimer, isPlaying } = useTimerContext();
   return (
     <>
       <nav>
@@ -25,20 +16,8 @@ export default function Home() {
       </nav>
       <main className="flex flex-col items-center justify-center gap-16">
         <TypeTags />
-        <CountdownCircleTimer
-          onComplete={() => {
-            setIsPlaying(false);
-            return { shouldRepeat: false };
-          }}
-          isPlaying={isPlaying}
-          size={360}
-          duration={23}
-          strokeWidth={32}
-          colors="#004777"
-        >
-          {renderTime}
-        </CountdownCircleTimer>
-        <Button onClick={() => setIsPlaying(true)} className="">
+        <Timer />
+        <Button onClick={toggleTimer} className="">
           {isPlaying ? "Give up" : "Start"}
         </Button>
       </main>
