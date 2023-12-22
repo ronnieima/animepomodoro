6b323b98ca518f19692737fd626e4c9ee56d9892
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-12-22 18:10:34 +0100

    remove pages

--- app/features/anime/animeSlice.ts
+++ app/features/anime/animeSlice.ts
@@ -1,18 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
-import {
-  Anime,
-  AnimeSearchParams,
-  JikanPagination,
-  JikanResponse,
-} from "@tutkli/jikan-ts";
 
 type StateType = {
-  page: number;
   searchQuery: string;
 };
 
 const initialState: StateType = {
-  page: 1,
   searchQuery: "",
 };
 
@@ -20,25 +12,11 @@ export const animeSlice = createSlice({
   name: "anime",
   initialState,
   reducers: {
-    incrementPage: (state) => {
-      state.page += 1;
-    },
-    decrementPage: (state) => {
-      if (state.page === 1) {
-        return;
-      }
-      state.page -= 1;
-    },
-    setPage: (state, action) => {
-      state.page = action.payload;
-    },
-
     updateSearchQuery: (state, action) => {
       state.searchQuery = action.payload;
     },
   },
 });
 
-export const { incrementPage, decrementPage, setPage, updateSearchQuery } =
-  animeSlice.actions;
+export const { updateSearchQuery } = animeSlice.actions;
 export default animeSlice.reducer;
