34bbbccc2a7cc76f56601c18ceab001611cdd149
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-12-22 20:58:25 +0100

    add main header

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,11 @@ export default function Home() {
   return (
     <main className=" h-full min-h-[100svh] ">
       <section className="flex flex-col items-center  justify-center gap-8 py-48">
-        <ThemeToggle />
+        <h1 className="text-7xl">Anime Pomodoro Timer</h1>
         <TypeTabs />
         <Timer />
         <TimerControl />
+        <ThemeToggle />
         <Stats />
       </section>
       <AnimeSection />
