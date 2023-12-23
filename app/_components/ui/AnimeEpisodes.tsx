62a04ef451ddc54a4a0d9ec5d35395ef600b2cab
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-12-23 18:24:29 +0100

    remove carousel

--- app/_components/ui/AnimeEpisodes.tsx
+++ app/_components/ui/AnimeEpisodes.tsx
@@ -1,24 +1,4 @@
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
-import { useQuery } from "@tanstack/react-query";
-import { Anime, AnimeEpisodeVideo, JikanResponse } from "@tutkli/jikan-ts";
-import Image from "next/image";
-import { Input } from "@/components/ui/input";
-import { Toggle } from "@/components/ui/toggle";
+import { Anime } from "@tutkli/jikan-ts";
 
 type AnimeEpisodesType = {
   selectedAnime: Anime;
