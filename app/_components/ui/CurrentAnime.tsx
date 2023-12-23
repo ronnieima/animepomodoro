c8e718dfb771bd4fb30eda5831c591eb81846496
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-12-23 15:40:53 +0100

    start on pick episode

--- app/_components/ui/CurrentAnime.tsx
+++ app/_components/ui/CurrentAnime.tsx
@@ -1,4 +1,10 @@
-import { Anime } from "@tutkli/jikan-ts";
+import { useQuery } from "@tanstack/react-query";
+import {
+  Anime,
+  AnimeEpisode,
+  AnimeEpisodeVideo,
+  JikanResponse,
+} from "@tutkli/jikan-ts";
 import Image from "next/image";
 import React from "react";
 
@@ -7,6 +13,22 @@ type CurrentAnimeType = {
 };
 
 function CurrentAnime({ selectedAnime }: CurrentAnimeType) {
+  const fetchAnime = async (): Promise<JikanResponse<AnimeEpisodeVideo[]>> => {
+    const response = await fetch(
+      `https://api.jikan.moe/v4/anime/${selectedAnime.mal_id}/videos/episodes`,
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  };
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["anime", selectedAnime.mal_id],
+    queryFn: fetchAnime,
+  });
+
+  console.log(data);
   return (
     <section className="grid w-full grid-cols-[1fr_2fr] pb-32">
       <div className="flex flex-col items-center justify-center ">
@@ -16,12 +38,17 @@ function CurrentAnime({ selectedAnime }: CurrentAnimeType) {
           alt={selectedAnime.title}
           height={200}
           width={300}
-          className="h-autoo w-[300px]"
+          className="h-auto w-[300px]"
         />
       </div>
 
       <div>
-        <p className="text-4xl font-semibold">{selectedAnime.title}</p>
+        <p className="text-4xl font-semibold">{`${selectedAnime.title_english} | ${selectedAnime.title_japanese}`}</p>
+        <div className="flex flex-wrap gap-16">
+          {data?.data?.map((episode) => (
+            <div key={episode.mal_id}>{episode.title}</div>
+          ))}
+        </div>
       </div>
     </section>
   );
