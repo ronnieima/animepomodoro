df3ade3549ae487065d5c13badb43a77ae3cc58f
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-02-17 11:00:56 +0100

    forgot an extra )

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -47,7 +47,6 @@ export default function Navbar() {
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
-            )
           </div>
         </>
       ) : (
